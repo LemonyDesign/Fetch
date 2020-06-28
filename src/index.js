4a8b5d3caa4f57ebf3394c62fecb8a3cd4203ea8
Mel Ashby <mel@lemonydesign.co.uk>
2020-06-28 12:07:34 +0100

    Async await

--- src/index.js
+++ src/index.js
@@ -9,21 +9,76 @@ document.getElementById("app").innerHTML = `
 </div>
 `;
 
-// FETCH
-// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-
-const SEARCH_QUERY = "Romance";
-const SEARCH_URL = "https://openlibrary.org/search.json?q=";
-const API_URL = `${SEARCH_URL}${SEARCH_QUERY}`;
-
+// BASIC FETCH
 // Potential errors include CORS error (Cross-origin) / permissions error
 // TypeError - request http but loaded over https
 
-fetch(API_URL)
-  .then(res => res.json()) // .json returns a promise
-  .then(data => {
+// FETCH
+// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+
+fetch("FETCH_URL")
+  .then(res => res.json()) // .json() returns a promise
+  .then(data => data)
+  .catch(err => err);
+
+// using async ... await
+// fetch() and json() return promises, so just use await keyword
+// try { const res = await fetch(url) } catch (e) { **_handle Error here_** }
+
+// IFFE immediately invoked function expression / self-invoking arrow function
+// wrap and scope code that automatically runs
+
+(async () => {
+  try {
+    const SEARCH_QUERY = "Romance";
+    const SEARCH_URL = "https://openlibrary.org/search.json?q=";
+    const API_URL = `${SEARCH_URL}${SEARCH_QUERY}`;
+
+    const res = await fetch(API_URL);
+    const data = await res.json();
     console.log(data);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+  } catch (error) {
+    console.error("Error message: ", error.message);
+  }
+})();
+
+// GET
+const getData = async () => {
+  try {
+    const SEARCH_QUERY = "Romance";
+    const SEARCH_URL = "https://openlibrary.org/search.json?q=";
+    const API_URL = `${SEARCH_URL}${SEARCH_QUERY}`;
+
+    const res = await fetch(API_URL);
+    let data = await res.json();
+    data = data.docs.map(doc => doc.title);
+    console.log(data);
+  } catch (error) {
+    console.error("Error message: ", error);
+  }
+};
+
+console.log(getData());
+
+// POST or 'PUT'
+// const userData = { username: "example" };
+
+// const postUser = async data => {
+//   const settings = {
+//     method: "POST",
+//     body: JSON.stringify(data),
+//     headers: {
+//       "Content-Type": "application/json"
+//     }
+//   };
+
+//   try {
+//     const res = await fetch("endpoint", settings);
+//     const responseData = await res.json();
+//     console.log(responseData);
+//   } catch (error) {
+//     console.error("Error message: ", error);
+//   }
+// };
+
+// console.log(postUser(userData));
